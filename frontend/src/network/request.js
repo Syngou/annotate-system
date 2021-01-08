5dc0b208d2de1abb466e47b8f8b1a16d5d8e71cc
Syngou <Syngou@outlook.com>
2021-01-08 13:14:39 +0800

    优化数据传输函数

--- frontend/src/network/request.js
+++ frontend/src/network/request.js
@@ -1,52 +1,44 @@
 import axios from "axios";
 
 export function request() {
-    const instance = axios.create({
-        // TODO：等待服务器
-        baseURL: "http://localhost:8080",
-        timeout: 5000,
-    });
-    return instance();
+  const instance = axios.create({
+    // TODO：等待服务器
+    baseURL: "http://localhost:8080",
+    timeout: 5000,
+  });
+  return instance();
 }
 
-export function addToDatabase(key, field, value) {
-    const instance = axios.create({
-        baseURL:
-            "http://localhost:8000/update?action=add&key=" +
-            key +
-            "&field=" +
-            field +
-            "&value=" +
-            value,
-        timeout: 5000,
-    });
-    return instance();
+export function addToRedis(key, field, value) {
+  const instance = axios.create({
+    baseURL: "http://localhost:8000/update",
+    params: {action: "add", key, field, value},
+    timeout: 5000,
+  });
+  return instance();
 }
 
-export function delFromDatabase(key, field) {
-    const instance = axios.create({
-        baseURL:
-            "http://localhost:8000/update?action=del&key=" +
-            key +
-            "&field=" +
-            field,
-        timeout: 5000,
-    });
-    return instance();
+export function deleteFromRedis(key, field) {
+  const instance = axios.create({
+    baseURL: "http://localhost:8000/update",
+    params: {action: "del", key, field},
+    timeout: 5000,
+  });
+  return instance();
 }
 
-export function postToDatabase(params) {
-    const instance = axios.create({
-        baseURL:
-            "http://localhost:8000/sql?relation=" +
-            params[0] +
-            "&name=" +
-            params[1] +
-            "&medicine=" +
-            params[2] +
-            "&tool=" +
-            params[3],
-        timeout: 5000,
-    });
-    return instance(params);
+export function postToBackend(params) {
+  const instance = axios.create({
+    baseURL:
+      "http://localhost:8000/sql?relation=" +
+      params[0] +
+      "&name=" +
+      params[1] +
+      "&medicine=" +
+      params[2] +
+      "&tool=" +
+      params[3],
+    timeout: 5000,
+  });
+  return instance(params);
 }
--- frontend/src/store/mutations.js
+++ frontend/src/store/mutations.js
@@ -1,78 +1,75 @@
-import {
-    addToDatabase,
-    delFromDatabase,
-    postToDatabase,
-} from "@/network/request";
+import {addToRedis, deleteFromRedis, postToBackend,} from "@/network/request";
+
 const mutations = {
-    addToList(state, info) {
-        let text = info.text;
-        if (info.id.indexOf("relation") !== -1) {
-            "list ==> " + state.relationsList;
-            state.relationsList.push(text);
+  addToList(state, info) {
+    let text = info.text;
+    if (info.id.indexOf("relation") !== -1) {
+      "list ==> " + state.relationsList;
+      state.relationsList.push(text);
 
-            addToDatabase("relation", info.id, text);
-        } else if (info.id.indexOf("name") !== -1) {
-            state.nameList.push(text);
+      addToRedis("relation", info.id, text);
+    } else if (info.id.indexOf("name") !== -1) {
+      state.nameList.push(text);
 
-            addToDatabase("name", info.id, text);
-        } else if (info.id.indexOf("medicine") !== -1) {
-            state.medicineList.push(text);
+      addToRedis("name", info.id, text);
+    } else if (info.id.indexOf("medicine") !== -1) {
+      state.medicineList.push(text);
 
-            addToDatabase("medicine", info.id, text);
-        } else if (info.id.indexOf("tool") !== -1) {
-            state.toolsList.push(text);
+      addToRedis("medicine", info.id, text);
+    } else if (info.id.indexOf("tool") !== -1) {
+      state.toolsList.push(text);
 
-            addToDatabase("tool", info.id, text);
+      addToRedis("tool", info.id, text);
+    }
+
+    postToBackend([
+      state.relationsList,
+      state.nameList,
+      state.medicineList,
+      state.toolsList,
+    ]);
+  },
+  // 删除list中的标注记录
+  deleteAnnotatedText(state, info) {
+    if (info.type.indexOf("relation") !== -1) {
+      for (let i = 0; i < state.relationsList.length; i++) {
+        if (state.relationsList[i] == info.text) {
+          state.relationsList.splice(i, 1);
+          console.log(true);
+          break;
         }
+      }
 
-        postToDatabase([
-            state.relationsList,
-            state.nameList,
-            state.medicineList,
-            state.toolsList,
-        ]);
-    },
-    // 删除list中的标注记录
-    deleteAnnotatedText(state, info) {
-        if (info.type.indexOf("relation") !== -1) {
-            for (let i = 0; i < state.relationsList.length; i++) {
-                if (state.relationsList[i] == info.text) {
-                    state.relationsList.splice(i, 1);
-                    console.log(true);
-                    break;
-                }
-            }
-
-            delFromDatabase("relation", info.deleteText);
-        } else if (info.type.indexOf("name") !== -1) {
-            for (let i = 0; i < state.nameList.length; i++) {
-                if (state.nameList[i] === info.text + "") {
-                    state.nameList.splice(i, 1);
-                    break;
-                }
-            }
-
-            delFromDatabase("name", info.deleteText);
-        } else if (info.type.indexOf("medicine") !== -1) {
-            for (let i = 0; i < state.medicineList.length; i++) {
-                if (state.medicineList[i] === info.text + "") {
-                    state.medicineList.splice(i, 1);
-                    break;
-                }
-            }
-
-            delFromDatabase("medicine", info.deleteText);
-        } else if (info.type.indexOf("tool") !== -1) {
-            for (let i = 0; i < state.toolsList.length; i++) {
-                if (state.toolsList[i] === info.text + "") {
-                    state.toolsList.splice(i, 1);
-                    break;
-                }
-            }
-
-            delFromDatabase("tool", info.deleteText);
+      deleteFromRedis("relation", info.deleteText);
+    } else if (info.type.indexOf("name") !== -1) {
+      for (let i = 0; i < state.nameList.length; i++) {
+        if (state.nameList[i] === info.text + "") {
+          state.nameList.splice(i, 1);
+          break;
         }
-    },
+      }
+
+      deleteFromRedis("name", info.deleteText);
+    } else if (info.type.indexOf("medicine") !== -1) {
+      for (let i = 0; i < state.medicineList.length; i++) {
+        if (state.medicineList[i] === info.text + "") {
+          state.medicineList.splice(i, 1);
+          break;
+        }
+      }
+
+      deleteFromRedis("medicine", info.deleteText);
+    } else if (info.type.indexOf("tool") !== -1) {
+      for (let i = 0; i < state.toolsList.length; i++) {
+        if (state.toolsList[i] === info.text + "") {
+          state.toolsList.splice(i, 1);
+          break;
+        }
+      }
+
+      deleteFromRedis("tool", info.deleteText);
+    }
+  },
 };
 
 export default mutations;
