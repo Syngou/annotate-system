30873daa8fc9a5f96e2e109c94f02614bd61a1f9
Syngou <Syngou@outlook.com>
2021-02-28 18:54:15 +0800

    修改动画

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <router-view />
+    <keep-alive>
+      <router-view />
+    </keep-alive>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
