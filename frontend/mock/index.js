817ce23b5034b4d6cbce4b88b30a39221f3ae0cb
Syngou <Syngou@outlook.com>
2021-03-27 19:41:07 +0800

    添加requiments.txt

--- frontend/mock/index.js
+++ frontend/mock/index.js
@@ -6,12 +6,7 @@ const table = require("./table");
 
 const mocks = [...user, ...table];
 
-// for front mock
-// please use it cautiously, it will redefine XMLHttpRequest,
-// which will cause many of your third-party libraries to be invalidated(like progress event).
 function mockXHR() {
-  // mock patch
-  // https://github.com/nuysoft/Mock/issues/300
   Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send;
   Mock.XHR.prototype.send = function () {
     if (this.custom.xhr) {
@@ -29,7 +24,6 @@ function mockXHR() {
       let result = null;
       if (respond instanceof Function) {
         const { body, type, url } = options;
-        // https://expressjs.com/en/4x/api.html#req
         result = respond({
           method: type,
           body: JSON.parse(body),
--- frontend/src/views/group/index.vue
+++ frontend/src/views/group/index.vue
@@ -9,5 +9,3 @@ export default {
   name: "Group",
 };
 </script>
-
-<style></style>
