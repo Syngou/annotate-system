13c631443f109f9a9d87edfd45a9bca4dd367a96
曾培鑫 <893522573@qq.com>
2021-08-21 14:51:25 +0800

    初步设计关系标注，问题比较多，思路应该可行，先提交一下

--- frontend/src/utils/annotateUtils.js
+++ frontend/src/utils/annotateUtils.js
@@ -1,4 +1,5 @@
 import store from "@/store/index";
+//import { some } from "core-js/core/array";
 /**
  *  按钮样式
  */
@@ -15,9 +16,85 @@ function textStyle(index) {
           background-color:${store.state.annotate.labels[index].color};
           border-radius: 10px;`;
 }
+
 /**
  *
- *  标注
+ *  关系标注
+ */
+export function relationAnnotateUtil(id, order) {
+  let text = window.getSelection().toString();
+
+  // 按钮样式
+  // TODO：样式美化
+  // 标注文本样式
+  let annotatedTestStyle = `border:5px solid #A3B9D9;
+                            background-color:#A3B9D9;
+                            border-radius: 10px;`;
+  // 选中不为空
+  if (text.length > 0) {
+    // 获取起始索引和结束索引
+    let annotateContent = document.querySelector("#annotateContent");
+    const range = window.getSelection().getRangeAt(0);
+    const preSelectionRange = range.cloneRange();
+    preSelectionRange.selectNodeContents(annotateContent);
+    preSelectionRange.setEnd(range.startContainer, range.startOffset);
+
+    let span = document.createElement("span");
+    span.setAttribute("style", annotatedTestStyle);
+    if(order)
+      span.setAttribute("id", toString(id[1])+"second")
+    else
+      span.setAttribute("id", toString(id[0])+"first")
+
+    let TextRange = window.getSelection().getRangeAt(0);
+    TextRange.surroundContents(span);
+    //span.appendChild(button);
+    //移除选中状态，否则很难看
+    window.getSelection().removeAllRanges();
+    //store.state.annotate.id++;
+  }
+}
+// 两段标注文本之间连线
+export function relationMatchingUtil(position1, position2, id) {
+  let draw = document.createElement("canvas");
+  console.log(position1);
+  console.log(position2);
+  let posXmin, posYmin;
+  if(position1[0] <= position2[0])
+    posXmin = position1[0];
+  else
+    posXmin = position2[0];
+  if(position1[1] <= position2[1])
+    posYmin = position1[1];
+  else
+    posYmin = position2[1];
+
+  let node = document.getElementById(toString(id[0])+"first");
+  console.log(node)
+  node.appendChild(draw);
+  draw.style.position = "relative";
+  draw.style.zIndex = 5;
+  draw.style.left = `0px`;
+  draw.style.top = `0px`;
+  draw.style.width = `${position2[0]+position1[0]-2*posXmin}px`;
+  draw.style.height = `${position2[1]+position1[1]-2*posYmin+40}px`;
+  let oneLine = draw.getContext("2d");
+  //console.log(position1);
+  oneLine.moveTo(10, 100);
+  oneLine.lineTo(10, 5);
+  oneLine.stroke();
+  oneLine.moveTo(10, 5);
+  oneLine.lineTo(position2[0]+position1[0]-2*posXmin, 5);
+  oneLine.stroke();
+  oneLine.moveTo(position2[0]+position1[0]-2*posXmin, 5);
+  oneLine.lineTo(position2[0]+position1[0]-2*posXmin, 100);
+  oneLine.stroke();
+
+  //console.log("hhhhh")
+}
+/**
+ *
+ *  实体标注
  * @param {string} id 给button标签的id，用于删除时查找
  * @param {number} index 标注颜色索引
  */
@@ -61,6 +138,7 @@ export function annotateUtil(id, index) {
     button.setAttribute("style", buttonStyle());
     let span = document.createElement("span");
     span.setAttribute("style", annotatedTestStyle);
+    
 
     let TextRange = window.getSelection().getRangeAt(0);
     TextRange.surroundContents(span);
